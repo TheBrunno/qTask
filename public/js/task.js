de7468b937462c34540d239fc87a8577bff94fac
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2023-01-17 20:15:07 -0300

    csrf token bug fix

--- public/js/task.js
+++ public/js/task.js
@@ -1,17 +1,20 @@
 class TaskValidator{
   async getAllValues(){
     this.name = document.querySelector('input[name="taskname"]').value;
+    this._csrf = document.querySelector('input[name="_csrf"').value;
     this.description = document.querySelector('textarea[name="description"]').value;
     this.getPriority();
     await this.send();
   }
   async send(){
     const data = {
+      _csrf: this._csrf,
       taskname: this.name,
       description: this.description,
       priority: this.priority
     }
     const url = '/mytasks/create';
+
     axios.post(url, data)
     .then(async res => {
       console.log(res.data);
